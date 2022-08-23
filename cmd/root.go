167c5f1f9f831a92ad0e5505c46d7edf86a3d5d9
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-08-22 20:52:26 -0700

    consilidating variables in root

--- cmd/root.go
+++ cmd/root.go
@@ -7,13 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.0.1"
-
-var rootCmd = &cobra.Command{
-	Use:     "task",
-	Version: version,
-	Short:   "\nTask is a CLI to help you manage tasks",
-}
+var (
+	version = "v0.0.1"
+	rootCmd = &cobra.Command{
+		Use:     "task",
+		Version: version,
+		Short:   "\nTask is a CLI to help you manage tasks",
+	}
+)
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
