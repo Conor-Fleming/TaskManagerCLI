1e925cdc018fbf49ab3d4e9aad12973f2cf55d3d
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-08-23 19:01:08 -0700

    ugh

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -13,9 +16,9 @@ var (
 	}
 )
 
-/*func Execute() {
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}*/
+}
